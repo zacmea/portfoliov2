ea255a286fef407406bc3bdf88af9e50209d37f3
zacmea <zach.mears@gmail.com>
2024-08-27 16:40:11 -0400

    adjusts vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/portfoliov2/",
+    base: '/portfoliov2/',
     plugins: [vue()],
 });
