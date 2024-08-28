98a595fd1cafe05b1ae12ab286de3bd91256a095
zacmea <zach.mears@gmail.com>
2024-08-28 18:26:35 -0400

    more deployment tries

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/portfoliov2/',
+    base: '/zacmea.github.io/',
     plugins: [vue()],
 });
